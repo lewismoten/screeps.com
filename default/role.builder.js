172a7476db0e2176e52fb9435692fd5d4ac35092
Lewis Moten <lewismoten@gmail.com>
2016-07-30 19:20:22 -0400

    Build closest construcution site

--- default/role.builder.js
+++ default/role.builder.js
@@ -16,11 +16,10 @@ var roleBuilder = {
         }
 
         if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
+            
+            let target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+            if(creep.build(target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
             }
         }
         else {
